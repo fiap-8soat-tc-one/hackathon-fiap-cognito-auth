41100b52759d7c4fd0ef05732f1865cd7a3c4052
jcmdsbr <jc_mds@outlook.com>
2025-04-01 20:11:15 -0300

    feat: added pipeline

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 913524932573.dkr.ecr.us-east-1.amazonaws.com
-#aws ecr create-repository --repository-name auth-lambda-repo
+aws ecr create-repository --repository-name auth-lambda-repo
 
 docker buildx build --platform linux/amd64 --provenance=false -t docker-image:test .
 docker tag docker-image:test  913524932573.dkr.ecr.us-east-1.amazonaws.com/auth-lambda-repo:latest
+docker tag docker-image:test  913524932573.dkr.ecr.us-east-1.amazonaws.com/auth-lambda-repo:${EXECUTION__DATE}
+docker push 913524932573.dkr.ecr.us-east-1.amazonaws.com/auth-lambda-repo:${EXECUTION__DATE}
 docker push 913524932573.dkr.ecr.us-east-1.amazonaws.com/auth-lambda-repo:latest
